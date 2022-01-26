41208b141f4e34c88436ae624be0f04d45700a48
Deorigosa <Deorigosa@users.noreply.github.com>
2022-01-26 13:53:38 +0300

    fix ouath???

--- config/initializers/slack_ruby_client.rb
+++ config/initializers/slack_ruby_client.rb
@@ -1,5 +1,5 @@
 require 'slack'
 
 Slack.configure do |config|
-  config.token = "xoxb-2604763146227-2980952625255-4k3f0TyhWZjHNjVvNzAhRN8q"
+  config.token = "xoxb-2604763146227-2980952625255-15rn5YNbpMTIAI24YdRrYrsJ"
 end
