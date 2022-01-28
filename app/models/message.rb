8ea251a03ba12ceb7578c74f7b1d0b29d671d4ed
Deorigosa <Deorigosa@users.noreply.github.com>
2022-01-28 13:42:00 +0300

    add aasm status to message

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,6 +1,22 @@
 # frozen_string_literal: true
 
 class Message < ApplicationRecord
+  include AASM
+
+  aasm column: 'status' do
+    state :pending, initial: true
+    state :sent
+    state :error
+
+    event :acknowledge_sent do
+      transitions from: :pending, to: :sent
+    end
+
+    event :fail do
+      transitions from: :pending, to: :error
+    end
+  end
+
   store_accessor :payload
 
   def serialize
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_26_093656) do
+ActiveRecord::Schema.define(version: 2022_01_28_103157) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2022_01_26_093656) do
     t.json "payload"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "status"
   end
 
 end
